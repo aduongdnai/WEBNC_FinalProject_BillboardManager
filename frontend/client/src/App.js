3e86b4f2e1a4f91d62581da5797f915d98d83b82
PhuocNhu <nguyennhuphuoc2002@gmail.com>
2024-01-01 20:48:08 +0700

    add sidebar

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -7,19 +7,22 @@ import {
 } from "react-router-dom";
 import ManageWardAndDistrict from "./components/ManageWardAndDistrict";
 import NavBar from "./components/NavBar";
-
+import Sidebar from "./components/Sidebar/Sidebar";
+import { Flex } from "@chakra-ui/react";
 
 
 function App() {
   return (
-    <Router className="App">
-      <NavBar/>
+    <div style={{display:"flex"}}>
+      <Sidebar/>
+    <Router>
       <Routes>
         <Route path="/map" element={<Map/>}/>
         <Route path="/manage" element={<ManageWardAndDistrict/>}/>
         <Route path="/" element={<Navigate replace to="/map"/>}/>
       </Routes>
     </Router>
+    </div>
   );
 }
 
