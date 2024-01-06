1e44cb7f08ea351665ca52bb378c28151eff0cce
nguyen tran dai duong <87960153+aduongdnai@users.noreply.github.com>
2024-01-06 18:06:37 +0700

    Update FilterOverlay component with new buttons and labels

--- frontend/citizen/src/components/FilterOverlay/index.js
+++ frontend/citizen/src/components/FilterOverlay/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, FormControl, FormLabel, Switch } from '@chakra-ui/react';
+import { Box, Button, FormControl, FormLabel, Spacer, Switch } from '@chakra-ui/react';
 function FilterOverlay({ onFilterChange }) {
     const [planned, setPlanned] = React.useState(false);
     const [reported, setReported] = React.useState(false);
@@ -12,17 +12,19 @@ function FilterOverlay({ onFilterChange }) {
         <Box position="absolute" bottom={0} right={0} p={4} backgroundColor="rgba(255, 255, 255, 0.8)">
             <FormControl display="flex" alignItems="center">
                 <FormLabel htmlFor="planned-switch" mb="0">
-                    Planned
+                    Chưa quy hoạch
                 </FormLabel>
+                <Spacer></Spacer>
                 <Switch id="planned-switch" isChecked={planned} onChange={(e) => setPlanned(e.target.checked)} />
             </FormControl>
             <FormControl display="flex" alignItems="center">
                 <FormLabel htmlFor="reported-switch" mb="0">
-                    Reported
+                    Báo cáo
                 </FormLabel>
+                <Spacer></Spacer>
                 <Switch id="reported-switch" isChecked={reported} onChange={(e) => setReported(e.target.checked)} />
             </FormControl>
-            <button onClick={handleFilterChange}>Apply Filters</button>
+            <Button colorScheme='blue' size='sm' mt="2" onClick={handleFilterChange}>Apply Filters</Button>
         </Box>
     );
 }
