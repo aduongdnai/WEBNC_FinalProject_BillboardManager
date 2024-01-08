133ea034fd93ab8e50ce7142422ff38b383b1b46
lequan <lequan2002.ql@gmail.com>
2024-01-08 17:48:13 +0700

    socket notify new report

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -14,8 +14,35 @@ import AdLocationPage from "./components/adLocation/AdLocationPage";
 import AdBoardsDisplay from "./components/AdBoard/AdBoardsDisplay";
 import ViewAndReviewRequests from "./components/adLocation/ViewAndReviewRequests";
 import ReportDashboard from "./components/ReportDashboard";
+import io from 'socket.io-client';
+import { useEffect } from "react";
+import { useToast } from '@chakra-ui/react';
+
+
+const socket = io('http://127.0.0.1:5000');
+
 
 function App() {
+  const toast = useToast();
+  
+  useEffect(() => {
+    // Simulate authentication with a secret token
+    socket.emit('authenticate', 'CLIENT');
+
+    // Listen for messages from the server
+    socket.on('notification', (data) => {
+      console.log('Received notification:', data);
+      
+      toast({
+        title: `Báo cáo mới`,
+        description: `Bạn có báo cáo mới từ ${data.senderName}, \nKiểu loại: ${data.reportType}, \nKhu vực: ${data.area}`,
+        duration: 5000,
+        isClosable: true,
+        variant: 'left-accent',
+        position: 'bottom-right',
+      });
+    });
+  }, []);
   return (
     <UserProvider>
       <div style={{ display: "flex" }}>
